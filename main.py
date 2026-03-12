1ecf283a4946e38cc47bcb72b1995506e6df271a
Hakimov Jaloliddinxon <profiy70@gmail.com>
2026-03-12 23:12:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3308,7 +3308,7 @@ def webhook():
         json_string = request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
-        return '', 200
+        return 'OK', 200
     else:
         return 'Unsupported Media Type', 415
     
